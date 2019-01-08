479beddce8bb3ad398f954ded30c89d01a01df63
kmilocabrera <camilo.cabrera@walmart.com>
2019-01-08 12:09:54 -0300

    fix(example): finished logic

--- examples/kafka-producer/index.js
+++ examples/kafka-producer/index.js
@@ -12,6 +12,9 @@ app.on(app.events.APPLICATION_READY, async () => {
 
     const topic = config.get('kafkaTopic');
     const brokers = config.get('kafkaBrokers');
+    let send = 0,
+        sended = 0,
+        intervalRef;
 
     const kafkaProducer = new KafkaProducer({
         kafkaHost: brokers
@@ -22,6 +25,7 @@ app.on(app.events.APPLICATION_READY, async () => {
     productStream
         .on('data', async (product) => {
             try {
+                send++;
                 const data = await kafkaProducer.send([{
                     topic,
                     messages: JSON.stringify(product),
@@ -33,10 +37,22 @@ app.on(app.events.APPLICATION_READY, async () => {
                 logger.debug({
                     err: error // std serializer interpret the err property
                 })
+            } finally {
+                sended++;
             }
         })
         .on('end', () => {
             logger.info('Reading products.json was finished');
+
+            intervalRef = setInterval(() => {
+                logger.info('Checking if product sending is finished');
+                if (send === sended) {
+                    clearInterval(intervalRef);
+                    logger.info('All products were sended, bye bye :)');
+                    process.exit(0);
+                }
+                logger.info('Product sending is in process, checking again in one second');
+            }, 1000);
         });
 });
 
